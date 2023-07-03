9d86ea3df541fa6cff7e5c8e9b16f9058229bbcb
Austin Hale <93340060+aus36@users.noreply.github.com>
2023-07-03 17:28:28 -0500

    Added navigation

---
+++ app/pages/projects.tsx
@@ -0,0 +1,10 @@
+import Link from 'next/link'
+
+export default function Projects() {
+    return (
+        <body>
+            <h1 className="text-center">These are my projects</h1>
+            <Link href="/">Home</Link>
+        </body>
+    )
+  }
